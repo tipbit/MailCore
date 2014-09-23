26e19d20372416223c1cf23b353cf64f3710e16d
Ewan Mellor <ewan@tipbit.com>
2014-09-23 12:25:35 -0700

    Connect before calling mailsession_select_folder, so that we don't crash if self.folderSession is not yet set.

--- Source/CTCoreFolder.m
+++ Source/CTCoreFolder.m
@@ -1103,6 +1103,11 @@ int uid_list_to_env_list(clist * fetch_result, struct mailmessage_list ** result
 
 
 -(void)refreshFolder {
+    BOOL success = [self connect];
+    if (!success) {
+        return;
+    }
+
     mailsession_select_folder(self.folderSession, NULL);
     char path[MAX_PATH_SIZE];
     [self getUTF7String:path fromString:myPath];
